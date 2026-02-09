9e58ee9760013412fc291cd7855a09932d8cdf3a
PopAnynomous234 <annameeshwar@gmail.com>
2026-02-09 17:26:58 -0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,13 +4,13 @@ const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
 async function handleRequest(event) {
-	await scramjet.loadConfig();
-	if (scramjet.route(event)) {
-		return scramjet.fetch(event);
-	}
-	return fetch(event.request);
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+        return scramjet.fetch(event);
+    }
+    return fetch(event.request);
 }
 
 self.addEventListener("fetch", (event) => {
-	event.respondWith(handleRequest(event));
+    event.respondWith(handleRequest(event));
 });
